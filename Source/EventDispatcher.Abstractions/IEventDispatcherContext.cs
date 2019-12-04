619a646f186290c63b6d71a3df8d56fe7e6971ed
Daniel P. Doyle <thedanieldoyle@outlook.com>
2019-12-04 21:28:13 +0000

    Updated dependency which took away IEvent constraint.

--- Source/EventDispatcher.Abstractions/IEventDispatcherContext.cs
+++ Source/EventDispatcher.Abstractions/IEventDispatcherContext.cs
@@ -5,8 +5,8 @@ namespace EventDispatcher
 {
     public interface IEventDispatcherContext
     {
-        void Dispatch<TEvent>() where TEvent : IEvent;
+        void Dispatch<TEvent>();
 
-        Task DispatchAsync<TEvent>(CancellationToken cancellationToken = default(CancellationToken)) where TEvent : IEvent;
+        Task DispatchAsync<TEvent>(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
